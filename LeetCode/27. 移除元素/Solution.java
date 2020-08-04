5cb57fcc78cc5e7ec5782a6a4ff7270cfc28e06e
wankcn <wankcn@icloud.com>
2020-08-04 18:27:09 +0800

    27.移除元素

---
+++ LeetCode/27. 移除元素/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int n: nums){
+            if(n!=val){
+                nums[count++] = n;
+            }
+        }
+        return count;
+    }
+}
