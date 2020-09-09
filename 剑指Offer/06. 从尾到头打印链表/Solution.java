ffa0d7909448a032c4f54b27a42cb1fa2b004bef
wankcn <wankcn@icloud.com>
2020-09-10 03:27:49 +0800

    从尾部打印链表

---
+++ 剑指Offer/06. 从尾到头打印链表/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Deque<Integer> stack = new LinkedList<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
